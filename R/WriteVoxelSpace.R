ed6fccd101efadfc872a7fac55e2fbc5589fe144
Philippe Verley <philippe.verley@ird.fr>
2021-07-06 12:12:29 +0200

    R package initial import.

---
+++ R/WriteVoxelSpace.R
@@ -0,0 +1,23 @@
+#' Write a voxel file
+#'
+#' @docType methods
+#' @rdname writeVoxelSpace
+#' @description write a voxel file out of a \code{\link{VoxelSpace-class}} object.
+#' @param voxelSpace the object of class VoxelSpace to write
+#' @param outputFile The path where to write the voxel file.
+#' @include AMAPVoxClasses.R
+#' @seealso \code{\link{readVoxelSpace}}
+#' @export
+writeVoxelSpace <- function(voxelSpace, outputFile){
+
+  stopifnot(is.VoxelSpace(voxelSpace))
+
+  # write header
+  conn <- file(outputFile, open="w")
+  writeLines("VOXEL SPACE", conn)
+  writeLines(paste0("#", paste(names(voxelSpace@header@parameters), voxelSpace@header@parameters, sep=":")), conn)
+  close(conn)
+
+  # write voxels
+  utils::write.table(voxelSpace@voxels, outputFile, row.names=F, col.names=T, na="NaN",sep=" ", append=T, quote=F)
+}

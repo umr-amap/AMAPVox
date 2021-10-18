f284ac977257627d6b5176998487790a46b5169c
Philippe Verley <philippe.verley@ird.fr>
2021-10-18 22:30:32 +0200

    (R) Bugfix in plot-VoxelSpace so that it can plot variables that were not originally in the voxel space. rgl and other graphic libraries are suggested and not imported by default.

--- R/AMAPVox.R
+++ R/AMAPVox.R
@@ -28,12 +28,6 @@
 #' @importFrom data.table data.table fread :=
 #' @importFrom stringr str_match str_trim str_squish str_starts str_split
 #'   str_remove_all str_extract
-#' @importFrom rgl par3d plot3d bgplot3d
-#' @importFrom fields image.plot
-#' @importFrom rgl par3d plot3d bgplot3d
-#' @importFrom fields image.plot
-#' @importFrom grDevices hcl.colors
-#' @importFrom graphics par plot.new title
 #' @importFrom dplyr %>%
 #' @importFrom rvest read_html html_element html_nodes html_attr
 #' @importFrom rappdirs user_data_dir

f3a1a8855ea658299351eeefae30e47ede32ba54
Philippe Verley <philippe.verley@ird.fr>
2022-03-29 18:02:40 +0200

    (R) Created canopyHeigh function that returns canopy height model.

--- R/Canopy.R
+++ R/Canopy.R
@@ -24,9 +24,15 @@
 #' missing canopy cells can be interpreted as gaps. Conversely if both ground
 #' and canopy are missing for a i-j-cell, then it is inconclusive.
 #'
-#' Function `aboveCanopy` returns voxel index above canopy level (excluded).
+#' ## Above/below canopy
 #'
+#' Function `aboveCanopy` returns voxel index above canopy level (excluded).
 #' Function `belowCanopy` returns voxel index below canopy level (included).
+#'
+#' ## Canopy Height Model
+#'
+#' Function `canopyHeight` returns ground distance at canopy level, including
+#' gaps.
 #'
 #' @return [data.table::data.table-class] object with voxel index either
 #' below canopy, canopy level or above canopy
@@ -34,6 +40,8 @@
 #' @param vxsp a \code{\link{VoxelSpace-class}} object.
 #' @param hit.min a positive integer, minimum number of hit/echo in a voxel
 #' to consider it contains vegetation.
+#' @param ... additional parameters which will be passed to `canopy` function.
+#' So far only `hit.min` parameter.
 #'
 #' @seealso [AMAPVox::butterfly], [AMAPVox::ground]
 #'
@@ -63,12 +71,12 @@ canopy <- function(vxsp, hit.min = 1) {
 
 #' @rdname canopy
 #' @export
-belowCanopy <- function(vxsp, hit.min = 1) {
+belowCanopy <- function(vxsp, ...) {
 
   k.cnp <- i <- j <- k <- NULL # trick to avoid notes in R CMD check
 
   # create a i, j, k, k.cnp data.table
-  vx <- merge(vxsp@data[, list(i, j, k)], canopy(vxsp, hit.min),
+  vx <- merge(vxsp@data[, list(i, j, k)], canopy(vxsp, ...),
               by = c("i", "j"), suffixes = c("", ".cnp"))
   # extract i, j, k voxels for k <= k.cnp
   vx[k <= k.cnp, list(i, j, k)]
@@ -76,13 +84,32 @@ belowCanopy <- function(vxsp, hit.min = 1) {
 
 #' @rdname canopy
 #' @export
-aboveCanopy <- function(vxsp, hit.min = 1) {
+aboveCanopy <- function(vxsp, ...) {
 
   k.cnp <- i <- j <- k <- NULL # trick to avoid notes in R CMD check
 
   # create a i, j, k, k.cnp data.table
-  vx <- merge(vxsp@data[, list(i, j, k)], canopy(vxsp, hit.min),
+  vx <- merge(vxsp@data[, list(i, j, k)], canopy(vxsp, ...),
               by = c("i", "j"), suffixes = c("", ".cnp"))
   # extract i, j, k voxels for k > k.cnp
   vx[k > k.cnp, list(i, j, k)]
+}
+
+#' @rdname canopy
+#' @export
+canopyHeight <- function(vxsp, ...) {
+
+  # get ground
+  ground <- ground(vxsp)
+
+  # get canopy
+  canopy <- canopy(vxsp, ...)
+
+  # canopy height model
+  i <- j <- k <- ground_distance <- NULL # trick to avoid "no visible binding" note
+  chm <- merge(vxsp@data[canopy, list(i, j, ground_distance), on=list(i, j, k)],
+               vxsp@data[ground[!canopy], list(i, j, ground_distance)],
+               all = TRUE)
+
+  return ( chm )
 }

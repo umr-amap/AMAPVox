d1e24a6d551d87fe36becda9b1d14fb333309e97
Philippe Verley <philippe.verley@ird.fr>
2022-03-29 18:18:59 +0200

    Created groundElevation function that returns digital elevation model.

--- R/Ground.R
+++ R/Ground.R
@@ -5,7 +5,7 @@
 #'
 #' @description Extract ground layer from \code{\link{VoxelSpace-class}} object.
 #'
-#' # Ground layer
+#' ## Ground layer
 #'
 #' The ground layer is the set of voxels that are just above ground level. The
 #' bottom facet of the voxel must be above ground
@@ -14,20 +14,23 @@
 #' incomplete (the function returns a data.table with
 #' `nrow(ground(vxsp)) < prod(dim(vxsp)[1:2])`) for some voxel space.
 #'
-#' # Above ground
+#' ## Above/below ground
 #'
 #' Function `aboveGround` returns voxel index above ground layer (included).
-#'
-#' # Below ground
-#'
 #' Function `belowGround` returns voxel index below ground layer (excluded).
 #'
-#' # Ground energy
+#' ## Ground energy
 #'
 #' Function `groundEnergy` estimates fraction of light reaching the ground. It
 #' is computed as the ratio of entering beam section on potential beam section
 #' (beams that would have crossed a voxel if there were no vegetation in the
 #' scene). It requires variables *bsEntering* and *bsPotential*.
+#'
+#' ## Ground elevation
+#'
+#' Function `groundElevation` returns the elevation of the ground layer. It is
+#' provided as a check function, to make sure that AMAPVox
+#' *digital elevation model* is consistent with the one provided in input.
 #'
 #' @return [data.table::data.table-class] object with voxel index either
 #' below ground, ground level or above ground.
@@ -112,4 +115,18 @@ groundEnergy <- function(vxsp) {
   i <- j <- k <- bsEntering <- bsPotential <- NULL # trick to avoid "no visible binding" note
   vxsp@data[ground, list(i, j, ground_energy=bsEntering / bsPotential),
             on=list(i, j, k)]
+}
+
+#' @rdname ground
+#' @export
+groundElevation <- function(vxsp) {
+
+  # get ground
+  ground <- ground(vxsp)
+
+  # ground elevation
+  dem <- vxsp@data[ground, c("i", "j")]
+  dem[["elevation"]] <- getPosition(vxsp, vxsp@data[ground])[["z"]]
+
+  return (dem)
 }

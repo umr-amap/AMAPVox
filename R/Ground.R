084ad8cbd15b48fe000ef87ea4b09c0168b5c877
Philippe Verley <philippe.verley@ird.fr>
2022-03-29 15:08:18 +0200

    Created groundEnergy function.

--- R/Ground.R
+++ R/Ground.R
@@ -5,6 +5,8 @@
 #'
 #' @description Extract ground layer from \code{\link{VoxelSpace-class}} object.
 #'
+#' # Ground layer
+#'
 #' The ground layer is the set of voxels that are just above ground level. The
 #' bottom facet of the voxel must be above ground
 #' `ground_distance(voxel_center) >= dz/2` with dz the voxel size on z axis.
@@ -12,9 +14,20 @@
 #' incomplete (the function returns a data.table with
 #' `nrow(ground(vxsp)) < prod(dim(vxsp)[1:2])`) for some voxel space.
 #'
-#' Function `aboveGround` returns voxel index from ground layer and above.
+#' # Above ground
 #'
-#' Function `belowGround` returns voxel index below ground layer.
+#' Function `aboveGround` returns voxel index above ground layer (included).
+#'
+#' # Below ground
+#'
+#' Function `belowGround` returns voxel index below ground layer (excluded).
+#'
+#' # Ground energy
+#'
+#' Function `groundEnergy` estimates fraction of light reaching the ground. It
+#' is computed as the ratio of entering beam section on potential beam section
+#' (beams that would have crossed a voxel if there were no vegetation in the
+#' scene). It requires variables *bsEntering* and *bsPotential*.
 #'
 #' @return [data.table::data.table-class] object with voxel index either
 #' below ground, ground level or above ground.
@@ -83,4 +96,19 @@ aboveGround  <- function(vxsp) {
   # return i, j, k index of below ground voxels
   i<- j <- k <- ground_distance <- NULL # trick to avoid "no visible binding" note
   return ( vxsp@data[ground_distance >= (0.5 * dz), list(i, j, k)] )
+}
+
+#' @rdname ground
+#' @export
+groundEnergy <- function(vxsp) {
+
+  # get ground
+  ground <- ground(vxsp)
+
+  # bsEntering and bsPotential variables required
+  stopifnot(c("bsEntering", "bsPotential") %in% names(vxsp))
+
+  # ground energy
+  i<- j <- bsEntering <- bsPotential <- NULL # trick to avoid "no visible binding" note
+  vxsp@data[ground, list(i, j, ground_energy=bsEntering / bsPotential)]
 }

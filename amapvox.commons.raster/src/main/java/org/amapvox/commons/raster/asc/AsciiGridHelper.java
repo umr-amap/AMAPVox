627819490b3816c2db17b1c3a17f5d403201fd27
PhiLippE VerLeY <philippe.verley@ird.fr>
2024-01-30 10:33:10 +0100

    (Java) Handled NaN values in digital terrain model reader. (Java) (Java) Closes #13

--- amapvox.commons.raster/src/main/java/org/amapvox/commons/raster/asc/AsciiGridHelper.java
+++ amapvox.commons.raster/src/main/java/org/amapvox/commons/raster/asc/AsciiGridHelper.java
@@ -15,71 +15,86 @@ import java.io.IOException;
 /**
  *
  * @author Julien Heurtebize (julienhtbe@gmail.com)
+ * @author Philippe Verley (philippe.verley@ird.fr)
  */
 public class AsciiGridHelper {
 
-    
-    public static Raster readFromAscFile(File ascFile) throws Exception{
-        
+    public static Raster readFromAscFile(File ascFile) throws IOException {
+
         final String pathFile = ascFile.getAbsolutePath();
-        
-        
+
         String line;
-        
+
         try (BufferedReader reader = new BufferedReader(new FileReader(ascFile))) {
-            
-            int nbCols = Integer.valueOf(reader.readLine().split(" ", 2)[1].trim());
-            int nbRows = Integer.valueOf(reader.readLine().split(" ", 2)[1].trim());
-            float xLeftCorner = Float.valueOf(reader.readLine().split(" ", 2)[1].trim());
-            float yLeftCorner = Float.valueOf(reader.readLine().split(" ", 2)[1].trim());
-            float step = Float.valueOf(reader.readLine().split(" ", 2)[1].trim());
-            float noDataValue = Float.valueOf(reader.readLine().split(" ", 2)[1].trim());
-            
+
+            int nbCols = Integer.parseInt(reader.readLine().split(" ", 2)[1].trim());
+            int nbRows = Integer.parseInt(reader.readLine().split(" ", 2)[1].trim());
+            float xLeftCorner = Float.parseFloat(reader.readLine().split(" ", 2)[1].trim());
+            float yLeftCorner = Float.parseFloat(reader.readLine().split(" ", 2)[1].trim());
+            float step = Float.parseFloat(reader.readLine().split(" ", 2)[1].trim());
+            float noDataValue = parseFloat(reader.readLine().split(" ", 2)[1].trim());
+
             float[][] zArray = new float[nbCols][nbRows];
-            
-            float z;
-            
+
             int yIndex = 0;
-            
-            while((line = reader.readLine()) != null){
-                
+
+            while ((line = reader.readLine()) != null) {
+
                 line = line.trim();
                 String[] values = line.split(" ", -1);
-                if(values.length != nbCols){
-                    throw new Exception("nb columns different from ncols header value");
+                if (values.length != nbCols) {
+                    throw new IOException("number of columns differs from ncols header value");
                 }
-                
-                for(int xIndex=0;xIndex<values.length;xIndex++){
-                    
-                    z = Float.valueOf(values[xIndex]);
-                    if(z == noDataValue){
-                        z = Float.NaN;
-                    }
-                    zArray[xIndex][yIndex] = z;
-                    
+
+                for (int xIndex = 0; xIndex < values.length; xIndex++) {
+                    zArray[xIndex][yIndex] = parseFloat(values[xIndex], noDataValue);
                 }
-                
+
                 yIndex++;
             }
-            
+
             Raster terrain = new Raster(pathFile, zArray, xLeftCorner, yLeftCorner, step, nbCols, nbRows);
-        
+
             return terrain;
-            
-        } catch (IOException ex) {
-            throw ex;
         }
     }
-    
-    public static void write(File output, Raster raster, boolean invertY) throws IOException{
+
+    /**
+     * Float parsing function that handles lower-case NaN.
+     *
+     * @param s a string representation of a float value.
+     * @return a float value.
+     */
+    public static float parseFloat(String s) {
+
+        s = s.trim();
+        s = s.equalsIgnoreCase("nan") ? "NaN" : s;
+        return Float.parseFloat(s);
+    }
+
+    /**
+     * Returns a new float initialized to the value represented by the String or
+     * NaN if the float equals the no-data value.
+     * @param s
+     * @param nodata
+     * @return 
+     */
+    public static float parseFloat(String s, float nodata) {
+
+        float f = parseFloat(s);
+        return f == nodata ? Float.NaN : f;
+    }
+
+    public static void write(File output, Raster raster, boolean invertY) throws IOException {
         write(output, raster.getzArray(),
-                raster.getColNumber(), raster.getRowNumber(), 
+                raster.getColNumber(), raster.getRowNumber(),
                 raster.getxLeftLowerCorner(), raster.getyLeftLowerCorner(),
                 raster.getCellSize(), invertY);
     }
-    
+
     /**
      * Write the raster in ascii grid format (*.asc)
+     *
      * @param output Output file
      * @param zArray 2d (x,y) array containing z values
      * @param colNumber number of columns
@@ -88,49 +103,49 @@ public class AsciiGridHelper {
      * @param yLeftLowerCorner lower left y corner
      * @param cellSize size of a cell
      * @param invertY
-     * @throws IOException Throws an IOException when output path is invalid or other
+     * @throws IOException Throws an IOException when output path is invalid or
+     * other
      */
-    public static void write(File output, float[][] zArray, int colNumber, int rowNumber, float xLeftLowerCorner, float yLeftLowerCorner, float cellSize, boolean invertY) throws IOException{
-        
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))){
-            
+    public static void write(File output, float[][] zArray, int colNumber, int rowNumber, float xLeftLowerCorner, float yLeftLowerCorner, float cellSize, boolean invertY) throws IOException {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
+
             float noDataValue = -9999.000000f;
-            writer.write("ncols "+colNumber+"\n");
-            writer.write("nrows "+rowNumber+"\n");
-            writer.write("xllcorner "+xLeftLowerCorner+"\n");
-            writer.write("yllcorner "+yLeftLowerCorner+"\n");
-            writer.write("cellsize "+cellSize+"\n");
-            writer.write("nodata_value "+noDataValue+"\n");
-            
-            for(int j = 0;j<rowNumber;j++){
-            
+            writer.write("ncols " + colNumber + "\n");
+            writer.write("nrows " + rowNumber + "\n");
+            writer.write("xllcorner " + xLeftLowerCorner + "\n");
+            writer.write("yllcorner " + yLeftLowerCorner + "\n");
+            writer.write("cellsize " + cellSize + "\n");
+            writer.write("nodata_value " + noDataValue + "\n");
+
+            for (int j = 0; j < rowNumber; j++) {
+
                 StringBuilder stringBuilder = new StringBuilder();
 
-                for(int i = 0 ; i<colNumber ; i++){
+                for (int i = 0; i < colNumber; i++) {
 
                     int yIndex;
-                    if(invertY){
+                    if (invertY) {
                         yIndex = rowNumber - j - 1;
-                    }else{
+                    } else {
                         yIndex = j;
                     }
-                    
+
                     if (Float.isNaN(zArray[i][yIndex])) {
                         stringBuilder.append(noDataValue);
                     } else {
                         stringBuilder.append(zArray[i][yIndex]);
                     }
-                    
+
                     stringBuilder.append(" ");
                 }
 
-                writer.write(stringBuilder.toString()+"\n");
+                writer.write(stringBuilder.toString() + "\n");
             }
-            
-            
+
         } catch (IOException ex) {
-            throw new IOException("Cannot write dtm file "+output.getAbsolutePath(), ex);
+            throw new IOException("Cannot write dtm file " + output.getAbsolutePath(), ex);
         }
     }
-   
+
 }

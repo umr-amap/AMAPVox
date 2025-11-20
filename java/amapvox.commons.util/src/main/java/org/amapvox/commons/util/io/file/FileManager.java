8861a69ebf843ab16036e9fc8bb22bcf76d67de6
PhiLippE VerLeY <philippe.verley@ird.fr>
2025-11-20 23:47:37 +0100

    (Java) Add function removeExtension from filename

--- java/amapvox.commons.util/src/main/java/org/amapvox/commons/util/io/file/FileManager.java
+++ java/amapvox.commons.util/src/main/java/org/amapvox/commons/util/io/file/FileManager.java
@@ -16,51 +16,49 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class FileManager {
-    
-    public static void skipLines(BufferedReader reader, int number) throws IOException{
+
+    public static void skipLines(BufferedReader reader, int number) throws IOException {
         try {
-            for(int i=0;i<number;i++){
+            for (int i = 0; i < number; i++) {
                 reader.readLine();
             }
         } catch (IOException ex) {
             throw ex;
         }
     }
-    
-    public static int getLineNumber(InputStreamReader stream) throws IOException{
-        
+
+    public static int getLineNumber(InputStreamReader stream) throws IOException {
+
         int count = 0;
-        
-        try{
+
+        try {
 
             BufferedReader reader = new BufferedReader(stream);
-                    
+
             while ((reader.readLine()) != null) {
                 count++;
             }
-            
+
             //reader.close();
-            
             return count;
-            
 
         } catch (IOException ex) {
             throw new IOException("Cannot read file", ex);
         }
     }
-    
-    public static int getLineNumber(File file) throws IOException{
-        
+
+    public static int getLineNumber(File file) throws IOException {
+
         return getLineNumber(file.getAbsolutePath());
     }
-    
-    public static int getLineNumber(String path) throws IOException{
-        
+
+    public static int getLineNumber(String path) throws IOException {
+
         int count = 0;
         InputStream is;
         try {
             is = new BufferedInputStream(new FileInputStream(path));
-            
+
             byte[] c = new byte[1024];
 
             int readChars = 0;
@@ -73,70 +71,86 @@ public class FileManager {
                     }
                 }
             }
-            
+
             return count;
-            
+
         } catch (FileNotFoundException ex) {
             throw ex;
         } catch (IOException ex) {
             throw ex;
         }
     }
-    
-    public static String readHeader(String path){
-        
+
+    public static String readHeader(String path) {
+
         try {
-            
+
             BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
             String line = reader.readLine();
             reader.close();
-            
+
             return line;
-            
+
         } catch (FileNotFoundException ex) {
             return null;
         } catch (IOException ex) {
             return null;
         }
     }
-    
-    public static String readSpecificLine(String path, int lineNumber) throws IOException{
-        
+
+    public static String readSpecificLine(String path, int lineNumber) throws IOException {
+
         try {
-            
+
             BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
             int count = 0;
             String line = null;
-            
-            
-            do{
-                
+
+            do {
+
                 line = reader.readLine();
                 count++;
-            }while(count != lineNumber);
-            
+            } while (count != lineNumber);
+
             return line;
-            
+
         } catch (FileNotFoundException ex) {
             throw ex;
         } catch (IOException ex) {
             throw ex;
         }
     }
-    
-    public static String getExtension(File file){
-        
+
+    public static String getExtension(File file) {
+
         int dotIndex = file.getName().lastIndexOf(".");
-        
-        if(dotIndex == -1){
+
+        if (dotIndex == -1) {
             return "";
-        }else{
+        } else {
             String extension = file.getName().substring(dotIndex, file.getName().length());
 
             return extension;
         }
     }
-    
-    
-    
+
+    /**
+     * From https://www.baeldung.com/java-filename-without-extension
+     *
+     * @param filename
+     * @param removeAllExtensions
+     * @return
+     */
+    public static String removeFileExtension(String filename, boolean removeAllExtensions) {
+        if (filename == null || filename.isEmpty()) {
+            return filename;
+        }
+
+        String extPattern = "(?<!^)[.]" + (removeAllExtensions ? ".*" : "[^.]*$");
+        return filename.replaceAll(extPattern, "");
+    }
+
+    public static String removeFileExtension(String filename) {
+        return removeFileExtension(filename, false);
+    }
 }
